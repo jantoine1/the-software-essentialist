6a3fe2356b26fd0219759c8deec784f583f6698a
Jon Antoine <jantoine@antoinesolutions.com>
2023-08-21 18:33:59 -0500

    feat: added initial responsibilities and examples

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,81 @@
-
 describe('boolean calculator', () => {
+  // describe('knows boolean values', () => {
+  //   it('knows TRUE is true', () => {
 
-})
+  //   });
+
+  //   it('knows FALSE is false', () => {
+
+  //   });
+  // });
+
+  // describe('knows the NOT operator', () => {
+  //   it('knows NOT TRUE is false', () => {
+
+  //   });
+
+  //   it('knows NOT FALSE is true', () => {
+
+  //   });
+  // });
+
+  // describe('knows the AND operator', () => {
+  //   it('knows TRUE AND TRUE is true', () => {
+
+  //   });
+
+  //   it('knows TRUE AND FALSE is false', () => {
+
+  //   });
+
+  //   it('knows FALSE AND TRUE is false', () => {
+
+  //   });
+
+  //   it('knows FALSE AND FALSE is false', () => {
+
+  //   });
+  // });
+
+  // describe('knows the OR operator', () => {
+  //   it('knows TRUE OR TRUE is true', () => {
+
+  //   });
+
+  //   it('knows TRUE OR FALSE is true', () => {
+
+  //   });
+
+  //   it('knows FALSE OR TRUE is true', () => {
+
+  //   });
+
+  //   it('knows FALSE OR FALSE is false', () => {
+
+  //   });
+  // });
+
+  // // Operator precedence:
+  // // 1. NOT
+  // // 2. AND
+  // // 3. OR
+  // describe('knows how to combine operators respective of precedence', () => {
+  //   it('knows that TRUE OR TRUE OR TRUE AND FALSE is true', () => {
+
+  //   });
+
+  //   it('knows that TRUE OR FALSE AND NOT FALSE is true', () => {
+
+  //   });
+  // });
+
+  // describe('knows how to handle how parenthesis', () => {
+  //   it('knows that (TRUE OR TRUE OR TRUE) AND FALSE is false', () => {
+
+  //   });
+
+  //   it('knows that NOT (TRUE AND TRUE) is false', () => {
+
+  //   });
+  // });
+});
