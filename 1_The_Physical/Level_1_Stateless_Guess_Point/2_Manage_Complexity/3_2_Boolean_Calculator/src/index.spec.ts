b4244cea48dd00814927ef4c2d19fabafe7d7637
Jon Antoine <jantoine@antoinesolutions.com>
2023-08-22 18:10:02 -0500

    feat: add a test for combining parenthesis with the OR operator

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -136,19 +136,20 @@ describe('boolean calculator', () => {
     });
   });
 
-  // describe('knows how to combine parenthesis and the OR operator', () => {
-  //   it.each([
-  //     ['(TRUE OR TRUE)', true],
-  //     ['(TRUE) OR FALSE', true],
-  //     ['FALSE OR (TRUE)', true],
-  //     ['(TRUE OR TRUE OR TRUE)', true],
-  //     ['(TRUE OR TRUE) OR FALSE', true],
-  //     ['TRUE OR (FALSE OR FALSE)', true],
-  //     ['FALSE OR (FALSE) OR FALSE', false],
-  //   ])('knows %s is %o', (str: string, expected: boolean) => {
-
-  //   });
-  // });
+  describe('knows how to combine parenthesis and the OR operator', () => {
+    it.each([
+      ['(TRUE OR TRUE)', true],
+      // ['(TRUE) OR FALSE', true],
+      // ['FALSE OR (TRUE)', true],
+      // ['(TRUE OR TRUE OR TRUE)', true],
+      // ['(TRUE OR TRUE) OR FALSE', true],
+      // ['TRUE OR (FALSE OR FALSE)', true],
+      // ['FALSE OR (FALSE) OR FALSE', false],
+    ])('knows %s is %o', (str: string, expected: boolean) => {
+      const result = booleanCalculator.calculate(str);
+      expect(result).toBe(expected);
+    });
+  });
 
   // describe('knows how to combine the NOT and OR operators', () => {
   //   it.each([
