10ead8272937c89c585836a910fec418f6fca2cc
Jon Antoine <jantoine@antoinesolutions.com>
2023-08-22 18:09:14 -0500

    feat: add another test for the OR operator

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -128,7 +128,7 @@ describe('boolean calculator', () => {
       ['FALSE OR TRUE OR TRUE', true],
       ['TRUE OR FALSE OR FALSE', true],
       ['FALSE OR TRUE OR FALSE', true],
-      // ['FALSE OR FALSE OR TRUE', true],
+      ['FALSE OR FALSE OR TRUE', true],
       // ['FALSE OR FALSE OR FALSE', false],
     ])('knows %s is %o', (str: string, expected: boolean) => {
       const result = booleanCalculator.calculate(str);
