3db427d2cb5a8f3ceb4330155c5407fe9a3fd81f
Jon Antoine <jantoine@antoinesolutions.com>
2023-08-22 18:08:59 -0500

    feat: add another test for the OR operator

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -125,7 +125,7 @@ describe('boolean calculator', () => {
       ['TRUE OR TRUE OR TRUE', true],
       ['TRUE OR TRUE OR FALSE', true],
       ['TRUE OR FALSE OR TRUE', true],
-      // ['FALSE OR TRUE OR TRUE', true],
+      ['FALSE OR TRUE OR TRUE', true],
       // ['TRUE OR FALSE OR FALSE', true],
       // ['FALSE OR TRUE OR FALSE', true],
       // ['FALSE OR FALSE OR TRUE', true],
