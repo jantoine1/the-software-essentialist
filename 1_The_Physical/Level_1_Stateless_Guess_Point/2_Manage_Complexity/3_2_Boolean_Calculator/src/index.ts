0435e25d62a26b52ce392943c2ba06d2bd0fcf67
Jon Antoine <jantoine@antoinesolutions.com>
2023-08-22 08:41:12 -0500

    feat: add test for multiple operators refactor: update the BooleanCalculator to skip processing OR operators untile AND operators have been processed

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -23,7 +23,7 @@ export class BooleanCalculator {
       }
       else if (part === 'OR') {
         operator = 'OR';
-        continue;
+        value = this.calculateParts(parts);
       }
 
       if (part === 'FALSE') {
