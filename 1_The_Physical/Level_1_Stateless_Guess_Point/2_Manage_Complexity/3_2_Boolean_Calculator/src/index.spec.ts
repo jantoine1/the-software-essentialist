f0c1010fae13ecf8d41548efae120808d00e5de9
Jon Antoine <jantoine@antoinesolutions.com>
2023-08-22 17:29:22 -0500

    added another test for the AND operator

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -59,7 +59,7 @@ describe('boolean calculator', () => {
       ['TRUE AND FALSE', false],
       ['FALSE AND TRUE', false],
       ['FALSE AND FALSE', false],
-      // ['TRUE AND TRUE AND TRUE', true],
+      ['TRUE AND TRUE AND TRUE', true],
       // ['TRUE AND TRUE AND FALSE', false],
       // ['TRUE AND FALSE AND TRUE', false],
       // ['FALSE AND TRUE AND TRUE', false],
