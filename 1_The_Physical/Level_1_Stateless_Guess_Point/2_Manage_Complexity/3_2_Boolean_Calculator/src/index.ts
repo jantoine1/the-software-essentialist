44d10b67dca050cbc61ab6f5ed0feda077593691
Jon Antoine <jantoine@antoinesolutions.com>
2023-08-21 20:10:42 -0500

    feat: added another case for the OR operator

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,35 +1,42 @@
 export class BooleanCalculator {
   calculate(str: string): boolean {
+    console.log(str);
     const strArray = str.split(/\s/);
     let result: boolean = true;
     let inverse: boolean = false;
     let operator: string = 'AND';
+    let value: boolean = true;
 
     for(const chunk of strArray) {
+      console.log(chunk);
       if (chunk === 'NOT') {
         inverse = !inverse;
+        continue;
       }
 
       if (chunk === 'OR') {
         operator = 'OR';
+        continue;
       }
 
       if (chunk === 'FALSE') {
-        if (operator === 'OR') {
-          result = result || false;
-
-          // Reset the operator.
-          operator = 'AND';
-        } else {
-          result = false;
-        }
-
-        break;
+        value = false;
       }
-    }
 
-    if (inverse) {
-      return !result;
+      if (inverse) {
+        value = !value;
+      }
+
+      if (operator === 'OR') {
+        result = result || value;
+      } else {
+        result = result && value;
+      }
+
+      // Reset the operator.
+      inverse = false;
+      operator = 'AND';
+      value = true;
     }
 
     return result;
