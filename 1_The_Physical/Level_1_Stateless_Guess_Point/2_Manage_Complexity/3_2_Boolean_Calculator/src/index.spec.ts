91a0738406544f5eed9a7686cd103d1c00335899
Jon Antoine <jantoine@antoinesolutions.com>
2023-08-22 17:28:47 -0500

    added another test for the AND operator

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -55,7 +55,7 @@ describe('boolean calculator', () => {
 
   describe('knows the AND operator', () => {
     it.each([
-      // ['TRUE AND TRUE', true],
+      ['TRUE AND TRUE', true],
       ['TRUE AND FALSE', false],
       // ['FALSE AND TRUE', false],
       // ['FALSE AND FALSE', false],
