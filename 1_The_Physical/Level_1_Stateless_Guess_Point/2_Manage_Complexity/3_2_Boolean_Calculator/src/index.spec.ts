a43a85c532221db67c2c623e6fc2fc359223210d
Jon Antoine <jantoine@antoinesolutions.com>
2023-08-22 14:30:19 -0500

    WIP

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -7,71 +7,105 @@ describe('boolean calculator', () => {
     booleanCalculator = new BooleanCalculator();
   })
 
-  describe('knows boolean values', () => {
-    it.each([
-      ['TRUE', true],
-      ['FALSE', false]
-    ])('knows %s is %o', (str: string, expected: boolean) => {
-      const result = booleanCalculator.calculate(str);
-      expect(result).toBe(expected);
-    });
-  });
+  // describe('knows boolean values', () => {
+  //   it.each([
+  //     ['TRUE', true],
+  //     ['FALSE', false]
+  //   ])('knows %s is %o', (str: string, expected: boolean) => {
+  //     const result = booleanCalculator.calculate(str);
+  //     expect(result).toBe(expected);
+  //   });
+  // });
 
-  describe('knows the NOT operator', () => {
-    it.each([
-      ['NOT TRUE', false],
-      ['NOT FALSE', true]
-    ])('knows %s is %o', (str: string, expected: boolean) => {
-      const result = booleanCalculator.calculate(str);
-      expect(result).toBe(expected);
-    });
-  });
+  // describe('knows how to handle how parenthesis', () => {
+  //   it.each([
+  //     ['(TRUE)', true],
+  //     ['(FALSE)', false],
+  //   ])('knows that %s is %o', (str: string, expected: boolean) => {
+  //     const result = booleanCalculator.calculate(str);
+  //     expect(result).toBe(expected);
+  //   });
+  // });
+
+  // describe('knows the NOT operator', () => {
+  //   it.each([
+  //     ['NOT TRUE', false],
+  //     ['NOT FALSE', true],
+  //     ['NOT (TRUE)', false],
+  //     ['NOT (FALSE)', true],
+  //     ['(NOT TRUE)', false],
+  //     ['(NOT FALSE)', true],
+  //     ['(NOT (TRUE))', false],
+  //     ['(NOT (FALSE))', true]
+  //   ])('knows %s is %o', (str: string, expected: boolean) => {
+  //     const result = booleanCalculator.calculate(str);
+  //     expect(result).toBe(expected);
+  //   });
+  // });
 
   describe('knows the AND operator', () => {
     it.each([
-      ['TRUE AND TRUE', true],
-      ['TRUE AND FALSE', false],
-      ['FALSE AND TRUE', false],
-      ['FALSE AND FALSE', false]
+      // ['TRUE AND TRUE', true],
+      // ['TRUE AND FALSE', false],
+      // ['FALSE AND TRUE', false],
+      // ['FALSE AND FALSE', false],
+      // ['(TRUE AND TRUE)', true],
+      // ['(TRUE AND FALSE)', false],
+      // ['(FALSE AND TRUE)', false],
+      // ['(FALSE AND FALSE)', false],
+      // ['(TRUE) AND (TRUE)', true],
+      ['(TRUE) AND (FALSE)', false],
+      // ['(FALSE) AND (TRUE)', false],
+      // ['(FALSE) AND (FALSE)', false],
+      // ['((TRUE) AND (TRUE))', true],
+      // ['((TRUE) AND (FALSE))', false],
+      // ['((FALSE) AND (TRUE))', false],
+      // ['((FALSE) AND (FALSE))', false],
     ])('knows %s is %o', (str: string, expected: boolean) => {
       const result = booleanCalculator.calculate(str);
       expect(result).toBe(expected);
     });
   });
 
-  describe('knows the OR operator', () => {
-    it.each([
-      ['TRUE OR TRUE', true],
-      ['TRUE OR FALSE', true],
-      ['FALSE OR TRUE', true],
-      ['FALSE OR FALSE', false]
-    ])('knows %s is %o', (str: string, expected: boolean) => {
-      const result = booleanCalculator.calculate(str);
-      expect(result).toBe(expected);
-    });
-  });
+  // describe('knows the OR operator', () => {
+  //   it.each([
+  //     ['TRUE OR TRUE', true],
+  //     ['TRUE OR FALSE', true],
+  //     ['FALSE OR TRUE', true],
+  //     ['FALSE OR FALSE', false]
+  //   ])('knows %s is %o', (str: string, expected: boolean) => {
+  //     const result = booleanCalculator.calculate(str);
+  //     expect(result).toBe(expected);
+  //   });
+  // });
 
-  // Operator precedence:
-  // 1. NOT
-  // 2. AND
-  // 3. OR
-  describe('knows how to combine operators respective of precedence', () => {
-    it.each([
-      ['TRUE OR TRUE OR TRUE AND FALSE', true],
-      ['TRUE OR FALSE AND NOT FALSE', true]
-    ])('knows that %s is %o', (str: string, expected: boolean) => {
-      const result = booleanCalculator.calculate(str);
-      expect(result).toBe(expected);
-    });
-  });
+  // // Operator precedence:
+  // // 1. NOT
+  // // 2. AND
+  // // 3. OR
+  // describe('knows how to combine operators respective of precedence', () => {
+  //   it.each([
+  //     ['TRUE OR TRUE OR TRUE AND FALSE', true],
+  //     ['TRUE OR FALSE AND NOT FALSE', true]
+  //   ])('knows that %s is %o', (str: string, expected: boolean) => {
+  //     const result = booleanCalculator.calculate(str);
+  //     expect(result).toBe(expected);
+  //   });
+  // });
 
   // describe('knows how to handle how parenthesis', () => {
-  //   it('knows that (TRUE OR TRUE OR TRUE) AND FALSE is false', () => {
-
+  //   it.each([
+  //     ['NOT TRUE', false],
+  //     ['NOT (TRUE)', false],
+  //     ['(TRUE OR TRUE OR TRUE) AND FALSE', false],
+  //     ['TRUE AND (TRUE OR ((FALSE OR TRUE) AND FALSE) OR FALSE) AND FALSE', true]
+  //   ])('knows that %s is %o', (str: string, expected: boolean) => {
+  //     const result = booleanCalculator.calculate(str);
+  //     expect(result).toBe(expected);
   //   });
 
-  //   it('knows that NOT (TRUE AND TRUE) is false', () => {
+  // //   it('knows that NOT (TRUE AND TRUE) is false', () => {
 
-  //   });
+  // //   });
   // });
 });
--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,6 +1,102 @@
 export class BooleanCalculator {
   calculate(str: string): boolean {
-    return this.calculateParts(str.split(/\s/));
+    let lChunks = str.split(/\(/).filter((e) => e);
+    console.log("lChunks ", lChunks);
+    lChunks = lChunks.map((lChunk) => {
+      lChunk = lChunk.trim();
+
+      if (lChunk === 'NOT') {
+        return lChunk;
+      }
+
+      let rChunks = lChunk.split(/\)/).filter((e) => e);
+      console.log("rChunks: ", rChunks);
+      rChunks = rChunks.map((rChunk) => {
+        rChunk = rChunk.trim();
+
+        if (rChunk === 'AND') {
+          return rChunk;
+        }
+
+        let oChunks = rChunk.split('OR').filter((e) => e);
+        console.log("oChunks: ", oChunks);
+        oChunks = oChunks.map((oChunk) => {
+          oChunk = oChunk.trim();
+
+          let aChunks = oChunk.split('AND').filter((e) => e);
+          console.log("aChunks: ", aChunks);
+
+          aChunks = aChunks.map((aChunk) => {
+            aChunk = aChunk.trim();
+
+            if (aChunk.startsWith('NOT')) {
+              if (aChunk.slice(4) === 'TRUE') {
+                aChunk = 'FALSE';
+              } else {
+                aChunk = 'TRUE';
+              }
+            }
+
+            return aChunk;
+          });
+          console.log("aChunks2: ", aChunks);
+
+          oChunk = aChunks.includes('FALSE') ? 'FALSE' : 'TRUE';
+
+          if (oChunk.startsWith('NOT')) {
+            if (oChunk.slice(4) === 'TRUE') {
+              oChunk = 'FALSE';
+            } else {
+              oChunk = 'TRUE';
+            }
+          }
+
+          return oChunk;
+        });
+        console.log("oChunks2: ", oChunks);
+
+        rChunk = oChunks.includes('TRUE') ? 'TRUE' : 'FALSE';
+
+        if (rChunk.startsWith('NOT')) {
+          if (rChunk.slice(4) === 'TRUE') {
+            rChunk = 'FALSE';
+          } else {
+            rChunk = 'TRUE';
+          }
+        }
+
+        return rChunk;
+      });
+      console.log("rChunks2: ", rChunks);
+
+      lChunk = rChunks.includes('FALSE') ? 'FALSE' : 'TRUE';
+
+      if (lChunk.startsWith('NOT')) {
+        if (lChunk.slice(4) === 'TRUE') {
+          lChunk = 'FALSE';
+        } else {
+          lChunk = 'TRUE';
+        }
+      }
+
+      return lChunk;
+    });
+    console.log("lChunks2: ", lChunks);
+
+    return this.calculatePart(lChunks.join(' '));
+  }
+
+  calculatePart(part: string): boolean {
+    if (part.startsWith('NOT')) {
+      if (part.slice(4) === 'TRUE') {
+        return false;
+      }
+    }
+    else if (part === 'FALSE') {
+      return false;
+    }
+  
+    return true;
   }
 
   calculateParts(parts: string[]): boolean {
